e69853df3e558179855d49c264ac69f45a4ef998
Guangchu Shi <rollschild@protonmail.com>
2024-09-23 20:27:24 -0400

    rewrote flake.nix - use flake utils

--- flake.nix
+++ flake.nix
@@ -1,49 +1,97 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+  description = "Git implemented in Python";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+    utils.url = "github:numtide/flake-utils";
+    utils.inputs.nixpkgs.follows = "nixpkgs";
+  };
   outputs =
     {
       self,
       nixpkgs,
       poetry2nix,
-    }:
-    let
-      supportedSystems = [
+      ...
+    }@inputs:
+    inputs.utils.lib.eachSystem
+      [
         "x86_64-linux"
         "x86_64-darwin"
         "aarch64-linux"
         "aarch64-darwin"
-      ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-    in
-    {
-      packages = forAllSystems (
+      ]
+      (
         system:
         let
-          inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryApplication;
+          # inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryApplication;
+          inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv;
+          pkgs = import nixpkgs { inherit system; };
         in
         {
-          default = mkPoetryApplication { projectDir = self; };
-        }
-      );
+          defaultPackage = poetry2nix.mkPoetryApplication {
+            projectDir = self;
+          };
 
-      devShells = forAllSystems (
-        system:
-        let
-          inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryEnv;
-        in
-        {
-          default = pkgs.${system}.mkShellNoCC {
-            packages = with pkgs.${system}; [
+          devShell = pkgs.mkShell rec {
+            name = "git-py";
+            packages = with pkgs; [
               (mkPoetryEnv { projectDir = self; })
               poetry
               ruff
               ruff-lsp
             ];
+            shellHook =
+              let
+                icon = "f121";
+              in
+              ''
+                export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+                export SHELL="${pkgs.zsh}/bin/zsh"
+              '';
           };
+
         }
+
       );
-    };
+  # {
+  # packages = forAllSystems (
+  # system:
+  # let
+  # inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryApplication;
+  # pkgs = import nixpkgs { inherit system; };
+  # llvm = pkgs.llvmPackages_18;
+  # lib = nixpkgs.lib;
+  # in
+  # {
+  # default = mkPoetryApplication { projectDir = self; };
+  # }
+  # );
+  #
+  # devShells = forAllSystems (
+  # system:
+  # let
+  # inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryEnv;
+  # in
+  # {
+  # default = pkgs.${system}.mkShellNoCC rec {
+  # name = "git-py";
+  # packages = with pkgs.${system}; [
+  # (mkPoetryEnv { projectDir = self; })
+  # poetry
+  # ruff
+  # ruff-lsp
+  # ];
+  # shellHook =
+  # let
+  # icon = "f121";
+  # in
+  # ''
+  # export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+  # export SHELL="${pkgs.zsh}/bin/zsh"
+  # '';
+  # };
+  # }
+  # );
+  # };
 }
